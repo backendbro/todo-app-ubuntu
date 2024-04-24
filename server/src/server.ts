40d2fff65fe8965c59db4e62b2339beefb3972b0
Ukagha Promise <85031254+backendbro@users.noreply.github.com>
2024-04-24 19:26:55 +0600

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,7 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!")
 })
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 const server = app.listen(port, () => {
     console.log(`server started on: localhost:${port}`)
 })
